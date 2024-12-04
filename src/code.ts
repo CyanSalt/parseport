6e943ae93cafdb5a77982cb63f50c1ac66aa8036
CyanSalt <c.salt@hotmail.com>
2024-12-04 20:37:14 +0800

    fix: transform json to ast

--- src/code.ts
+++ src/code.ts
@@ -1,18 +1,23 @@
-import type { Program } from '@babel/types'
+import type { Node } from '@babel/types'
+import { valueToNode } from '@babel/types'
 import { babelParse, getLang } from 'ast-kit'
 import { PARSEPORT_UNKNOWN, parseportNode } from './node'
 import type { ParseportOptions } from './types'
 
-export type ParseportParser = (code: string, file?: string, lang?: string) => Program | Promise<Program>
+export type ParseportParser = (code: string, file?: string, lang?: string) => Node | Promise<Node>
 
 export const defaultParser: ParseportParser = (code, file, lang) => {
+  if (lang === 'json') {
+    const value = JSON.parse(code)
+    return valueToNode(value)
+  }
   return babelParse(code, lang)
 }
 
 export async function parseportCode(code: string, options?: ParseportOptions) {
   const parser = options?.parser ?? defaultParser
   const lang = options?.lang ?? (options?.file ? getLang(options.file) : undefined)
-  let ast: Program
+  let ast: Node
   try {
     ast = await parser(code, options?.file, lang)
   } catch {
